78d33d5b7a4c86a0673ee2c6c9ec96e0f39250c6
Guido Marzocchini <guido.marzocchini@gmail.com>
2026-08-29 14:20:16 -0300

    agregue href al navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,22 @@
 const navItems = [
-  "Cartelera",
-  "Complejos",
-  "Experiencia",
-  "Novedades",
-  "Nosotros",
+  { label: "Cartelera", href: "#peliculas" },
+  { label: "Complejos", href: "#complejos" },
+  { label: "Experiencia", href: "#experiencia" },
+  { label: "Novedades", href: "#novedades" },
+  { label: "Nosotros", href: "#nosotros" },
 ];
 
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar__logo">
-         <img src="/logo.png" alt="Cinema La Plata" />
-      </div>
+      <a href="#inicio" className="navbar__logo">
+        <img src="/logo.png" alt="Cinema La Plata" />
+      </a>
 
       <nav className="navbar__links">
         {navItems.map((item) => (
-          <a key={item} href="#">
-            {item}
+          <a key={item.label} href={item.href}>
+            {item.label}
           </a>
         ))}
 
